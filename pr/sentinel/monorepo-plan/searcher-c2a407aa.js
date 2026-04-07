56eced076f00004143a0387edacbb7df4b91b606
higherorderfunctor <higherorderfunctor@users.noreply.github.com>
2026-04-07 17:19:31 +0000

    deploy: 1f1ad35206a1be8e0aff8599f7bfbe0642462d24

--- pr/sentinel/monorepo-plan/searcher-c2a407aa.js
+++ pr/sentinel/monorepo-plan/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ada647d3.js',
+                path_to_root + 'searchindex-d6551f1d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
