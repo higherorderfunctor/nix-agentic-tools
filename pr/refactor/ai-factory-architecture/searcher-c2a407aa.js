dc18a9a97679e91d2a6f28a3a46fab2c47a59c40
higherorderfunctor <higherorderfunctor@users.noreply.github.com>
2026-04-22 04:08:52 +0000

    deploy: 9eca7b09924b320646dfae9fe9ab31c2032e5f80

--- pr/refactor/ai-factory-architecture/searcher-c2a407aa.js
+++ pr/refactor/ai-factory-architecture/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d5bbbdfc.js',
+                path_to_root + 'searchindex-a02b8110.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
